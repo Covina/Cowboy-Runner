6549e82b30bd2f5b3f0165d6896398b19de3d8fd
Covina <jabs303@gmail.com>
2017-04-04 21:47:44 -0700

    Section 5, Lecture 84; Added player and obstacle destruction scripts.

---
+++ Assets/Scripts/Player Scripts/PlayerDied.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDied : MonoBehaviour {
+
+
+	public delegate void EndGame();
+	public static event EndGame endGame; 
+
+
+	void PlayerDiedEndGame ()
+	{
+		if (endGame != null) {
+
+			endGame();
+
+		}
+
+
+		Destroy(gameObject);
+
+	}
+
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		// Player hit the shredder offscreen
+		if (collider.tag == "Shredder") {
+			PlayerDiedEndGame();
+		}
+
+	}
+
+
+
+	void OnCollisionEnter2D (Collision2D collider)
+	{
+		// Player hit a Zombie and dies immediately
+		if (collider.gameObject.tag == "Zombie") {
+			PlayerDiedEndGame();
+		}
+
+	}
+
+
+
+} // PlayerDied
